326e938c3a48d4b68fee76b760f98d322cf136fc
GeovannaT05 <00154423@uca.edu.sv>
2024-06-24 23:01:16 -0600

    Pruba 1

---
+++ prueba.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main(){
+
+    int numero;
+
+    cout << " JUEGO DE PIERDA, PAPEL O TIJERA" << endl;
+    cout << " Seleccione una de las opciones" << endl;
+    cout << " 1. Piedra" << "2. Papel" << endl << "3. Tijera" << endl;
+    while (/* condition */)
+    {
+         if ()
+    }
+    
+
+    cout << " La Maquina eligio"
+    
+
+
+    return 0;
+}
