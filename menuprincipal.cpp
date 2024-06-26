d96096f0bb6130feb270b14a6ddd155230ebeee0
edwinalvarez04 <00078724@uca.edu.sv>
2024-06-26 00:44:41 -0600

    menu

--- menuprincipal.cpp
+++ menuprincipal.cpp
@@ -2,6 +2,35 @@
 using namespace std;
 
 int main(){
-cout << "hola mundo" << endl;
 
+int modo;
+
+cout << "BIENVENIDO A PIEDRA, PAPEL, O TIJERA" << endl;
+cout << " Selecciona una de las opciones" << endl;
+cout << " 1. Jugador contra maquina" << endl;
+cout << " 2. Jugador contra Jugador" << endl;
+cout << " 3. Como se juega?" << endl;
+cin >> modo;
+
+switch (modo)
+{
+case 1: 
+
+break;
+
+case 2:
+
+
+
+break;
+case 3:
+}
+
+cout << "El objetivo es vencer al oponente seleccionando el arma que gana, según las siguientes reglas:" << endl;
+cout << "La piedra aplasta la tijera. ..." << endl;
+cout << "La tijera corta el papel. ... " << endl;
+cout << "El papel envuelve la piedra. ..." << endl;
+cout << "En caso de empate (que dos jugadores elijan el mismo elemento o que tres jugadores elijan cada uno un objeto distinto), se juega otra vez." << endl;
+
+return 0;
 }
