d1b96709b5aa78f2c4575ef9fff64f867f5b3594
GeovannaT05 <00154423@uca.edu.sv>
2024-06-26 22:32:43 -0600

    Primer avance

--- prueba.cpp
+++ prueba.cpp
@@ -3,20 +3,38 @@
 using namespace std;
 int main(){
 
+    int computadora;
     int numero;
 
+   do{
     cout << " JUEGO DE PIERDA, PAPEL O TIJERA" << endl;
-    cout << " Seleccione una de las opciones" << endl;
+    cout << " Seleccione una de las opciones" << endl ;
     cout << " 1. Piedra" << "2. Papel" << endl << "3. Tijera" << endl;
-    while (/* condition */)
+    cin >> numero << endl;
+
+    
+    int piedra, papel, tijera ;
+    int puntosf = 0 , puntosc = 0 , empate = 0 ;
+
+   cout << " La Maquina eligio:" ;
+
+    }  while (/* condition */)
     {
-         if ()
+         if (numero == computadora){
+    cout << "Empate" ;
+}
+        else if ((numero == 1 && computadora == 3) ||
+                 (numero == 2 && computadora == 1) ||
+                 (numero == 3 && computadora == 2))
+    {
+        cout  << "El usuario gana" ;
+    }           
+        else {
+            cout << "Maquina gana"
+        }
+
+    
     }
-    
-
-    cout << " La Maquina eligio"
-    
-
 
     return 0;
 }
