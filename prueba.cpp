cf7c8e38acc3511d50c7d2167fb1fd06d9508b09
GeovannaT05 <00154423@uca.edu.sv>
2024-07-04 22:19:45 -0600

    Aavance

--- prueba.cpp
+++ prueba.cpp
@@ -1,40 +1,46 @@
 #include <iostream>
 #include <cstdlib>
 using namespace std;
+
 int main(){
 
     int computadora;
     int numero;
+    int contador;
+    int intentos;
 
    do{
     cout << " JUEGO DE PIERDA, PAPEL O TIJERA" << endl;
     cout << " Seleccione una de las opciones" << endl ;
-    cout << " 1. Piedra" << "2. Papel" << endl << "3. Tijera" << endl;
-    cin >> numero << endl;
+    cout << "1. Piedra" << endl << "2. Papel" << endl << "3. Tijera" << endl;
+    cin >> numero ;
+    cout << endl ;
+
+    computadora = rand() % 3 + 1 ;
+
+  cout <<  "La maquina elige: " << computadora << endl ;
 
     
-    int piedra, papel, tijera ;
-    int puntosf = 0 , puntosc = 0 , empate = 0 ;
+   /* int piedra, papel, tijera ;
+    int puntosf = 0 , puntosc = 0 , empate = 0 */
 
-   cout << " La Maquina eligio:" ;
 
-    }  while (/* condition */)
-    {
-         if (numero == computadora){
+     if (numero == computadora){
     cout << "Empate" ;
 }
         else if ((numero == 1 && computadora == 3) ||
                  (numero == 2 && computadora == 1) ||
                  (numero == 3 && computadora == 2))
     {
-        cout  << "El usuario gana" ;
-    }           
-        else {
-            cout << "Maquina gana"
-        }
-
-    
+        cout  << "Ganaste" ;
+    }  else {
+            cout << "Perdiste" ;
     }
+
+contador++;
+
+} while (intentos < 3) ;
+
 
     return 0;
 }
