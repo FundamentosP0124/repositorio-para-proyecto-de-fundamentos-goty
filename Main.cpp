91c4c0deaa1bd970d4131232bb6fc2f458a5c2e5
edwinalvarez04 <00078724@uca.edu.sv>
2024-07-08 11:35:34 -0600

    Union de todo

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
 using namespace std;
 
 int main(){
@@ -64,10 +65,34 @@ contador++;
 
 break;
 
-case 2:
+case 2: {
   cout << "Modo Jugador contra Jugador seleccionado." << endl;
-break;
+  void ganador(int decision1, int decision2, string jugador1, string jugador2);
+  int modo;
+    int decision1 = 0;
+    int decision2 = 0;
+    string player1, player2;
+cout << "Modo jugador contra jugador seleccionado" << endl << endl;
 
+            cout << "En el juego, el arma de cada jugador puede ser una de las 3:" << endl;
+            cout << "1. Piedra" << endl;
+            cout << "2. Papel" << endl;
+            cout << "3. Tijera" << endl << endl;
+
+            cout << "Jugador 1, ingresa tu nombre: ";
+            cin >> player1;
+            cout << "Jugador 1, escoge tu arma segun su numero: ";
+            cin >> decision1;
+
+            cout << "Jugador 2, ingresa tu nombre: ";
+            cin >> player2;
+            cout << "Jugador 2, escoge tu arma segun su numero: ";
+            cin >> decision2;
+
+            ganador(decision1, decision2, player1, player2);
+
+break;
+}
 case 3:
 cout << "El objetivo es vencer al oponente seleccionando el arma que gana, según las siguientes reglas:" << endl;
 cout << "- La piedra aplasta la tijera." << endl;
@@ -84,4 +109,19 @@ cout << "Opcion no valida, porfavor selecciona una opcion del 1 al 4" << endl;
 }
 
 return 0;
+}
+void ganador(int decision1, int decision2, string player1, string player2) {
+    if (decision1 == decision2) {
+        cout << "Es un empate" << endl;
+    } else if ((decision1 == 1 && decision2 == 3) ||
+               (decision1 == 3 && decision2 == 2) ||
+               (decision1 == 2 && decision2 == 1)) {
+        cout << player1 << " gana!" << endl;
+    } else if ((decision2 == 1 && decision1 == 3) ||
+               (decision2 == 3 && decision1 == 2) ||
+               (decision2 == 2 && decision1 == 1)) {
+        cout << player2 << " gana!" << endl;
+    } else {
+        cout << "Decisión no válida" << endl;
+    }
 }
