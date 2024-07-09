6015f272d420f1577ee6bd615f18db771efeb79a
GeovannaT05 <00154423@uca.edu.sv>
2024-07-08 21:17:51 -0600

    Final

--- prueba.cpp
+++ prueba.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
+#include <fstream>
+
 using namespace std;
 
 int main(){
@@ -9,37 +12,33 @@ int main(){
     int contador;
     int intentos;
 
-   do{
     cout << " JUEGO DE PIERDA, PAPEL O TIJERA" << endl;
-    cout << " Seleccione una de las opciones" << endl ;
-    cout << "1. Piedra" << endl << "2. Papel" << endl << "3. Tijera" << endl;
-    cin >> numero ;
-    cout << endl ;
+    //En la parte del registro se deifine "jugador1" //
+            do {
+                cout << jugador1 << " porfavor selecciona una de las opciones para vencer a la maquina" << endl;
+                cout << "1. Piedra" << endl << "2. Papel" << endl << "3. Tijera" << endl;
+                cin >> numero;
+                cout << endl;
 
-    computadora = rand() % 3 + 1 ;
+                computadora = rand() % 3 + 1;
+                cout << "La maquina elige: " << computadora << endl;
 
-  cout <<  "La maquina elige: " << computadora << endl ;
+                if (numero == computadora) {
+                    cout << "Empate" << endl;
+                  //Se guarda en el registro final //
+                    registro(jugador1 + " vs CPU: Empate");
+                } else if ((numero == 1 && computadora == 3) ||
+                           (numero == 2 && computadora == 1) ||
+                           (numero == 3 && computadora == 2)) {
+                    cout << "Ganaste!" << endl;
+                    registro(jugador1 + " vs CPU: Ganaste");
+                } else {
+                    cout << "Perdiste!" << endl;
+                    registro(jugador1 + " vs CPU: Perdiste");
+                }
 
-    
-   /* int piedra, papel, tijera ;
-    int puntosf = 0 , puntosc = 0 , empate = 0 */
-
-
-     if (numero == computadora){
-    cout << "Empate" ;
-}
-        else if ((numero == 1 && computadora == 3) ||
-                 (numero == 2 && computadora == 1) ||
-                 (numero == 3 && computadora == 2))
-    {
-        cout  << "Ganaste" ;
-    }  else {
-            cout << "Perdiste" ;
-    }
-
-contador++;
-
-} while (intentos < 3) ;
+                contador++;
+            } while (contador < intentos);
 
 
     return 0;
